25a54170ed127df4cb8fd01cf39f23654fa85300
hungfaikoo <jhk88@hotmail.com>
2019-09-05 11:25:17 -0700

    create models post and comments

---
+++ models/Post.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Comment = require('./Comment');
+const postSchema = new Schema({
+  username: {
+    types: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  title: {
+    type: String,
+    require: true
+  },
+  content: {
+    type: String,
+    require: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  comment: [Comment.schema],
+  city: {
+    type: String
+  }
+})
+
+const Post = mongoose.model('post', postSchema)
+module.exports = Post;
